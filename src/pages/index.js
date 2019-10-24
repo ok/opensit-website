6dea3959c2c5e546d73240c2a53eabd6563332df
Colin Krawchuk <colin@colingk.net>
2019-10-24 13:24:46 +0200

    fix mobile view on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,35 +10,37 @@ import { getYtThumbnailUrl } from "../components/yt-helper.js"
 const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <div className="row">
-      <div className="col-12">
-        <h2>Recent SAP Inside Tracks</h2>
+      <div className="row mb-3">
+        <div className="col-12">
+          <h2>Recent SAP Inside Tracks</h2>
+        </div>
       </div>
-    </div>
-    {data.gcms.events.map(event => (
-    <div className="row" style={{"margin-bottom" : "1rem"}} key={event.id}>
-      <div className="col-3 mb-3">
-        <div className="card">
-          <div className="card-body">
-            <Link to={`/${event.insideTrack.hashtag}`}><h5 className="card-title">{event.insideTrack.name}</h5></Link>
+      {data.gcms.events.map(event => (
+      <div className="row mb-5"  key={event.id}>
+        <div className="col-sm-12 col-md-3 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <Link to={`/${event.insideTrack.hashtag}`}><h5 className="card-title ok-title">{event.insideTrack.name}</h5></Link>
+            </div>
+            <div className="card-footer">
+                <small className="text-muted">Date: {Moment(event.date).format('D MMM YYYY')}</small>
+            </div>
           </div>
-          <div className="card-footer">
-              <small className="text-muted">Date: {Moment(event.date).format('D MMM YYYY')}</small>
+        </div>
+        <div className="col-sm-12 col-md-9">
+          <div className="card-deck">
+          {event.sessions.map(session => (
+           <div className="card ok-card " key={session.id}>
+           <img className="card-img-top" src={ getYtThumbnailUrl(session.recordingUrl) } alt="Video thumbnail"/>
+           <div className="card-body" key={session.id}>
+               <p className="card-text"><small className="text-muted">{session.title}</small></p>
+           </div>
+         </div>
+          ))}
           </div>
         </div>
       </div>
-      <div className="card-deck col-9">
-      {event.sessions.map(session => (
-        <div className="card" style={{width: `33%`,}} key={session.id}>
-          <img className="card-img-top" src={ getYtThumbnailUrl(session.recordingUrl) } alt="Video thumbnail"/>
-          <div className="card-body" key={session.id}>
-              <p className="card-text"><small className="text-muted">{session.title}</small></p>
-          </div>
-        </div>
       ))}
-      </div>
-    </div>
-    ))}
     </Layout>
 )
 
