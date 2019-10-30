f2372418df8e860c25999603f6042090ac0dd5bf
oliver <gokohl@gmail.com>
2019-10-30 21:29:47 +0100

    pushing Colin's changes manually

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,44 +7,49 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { getYtThumbnailUrl } from "../components/yt-helper.js"
 
-const IndexPage = ({data}) => (
-  <Layout>
+const IndexPage = ({ data }) => {
+  data.gcms.events.forEach(event => {
+    var eventDate = new Date(event.date)
+    event.year = eventDate.getFullYear()
+  })
+
+  return (
+    <Layout>
     <SEO title="Home" />
-    <div className="row mb-3">
-      <div className="col-12">
-        <h2>Recent SAP Inside Tracks</h2>
-      </div>
-    </div>
-    {data.gcms.events.map(event => (
-    <div className="row mb-5"  key={event.id}>
-      <div className="col-sm-12 col-md-3 mb-3">
-        <div className="card">
-          <div className="card-body">
-            <Link to={`/${event.insideTrack.hashtag}`}><h5 className="card-title ok-title">{event.insideTrack.name}</h5></Link>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">Date: {Moment(event.date).format('D MMM YYYY')}</small>
-          </div>
+      <div className="row mb-3">
+        <div className="col-12">
+          <h2>Recent SAP Inside Tracks</h2>
         </div>
       </div>
-      <div className="col-sm-12 col-md-9">
-        <div className="card-deck">
-        {event.sessions.map(session => (
-        <Link to={`/${event.insideTrack.hashtag}/${event.year}/${session.slug}`} key={session.id}>
-          <div className="card ok-card " key={session.id}>
-            <img className="card-img-top" src={ getYtThumbnailUrl(session.recordingUrl) } alt="Video thumbnail"/>
-            <div className="card-body" key={session.id}>
-                <p className="card-text"><small className="text-muted">{session.title}</small></p>
+      {data.gcms.events.map(event => (
+      <div className="row mb-5" key={event.id}>
+        <div className="col-sm-12 col-md-3 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <Link to={`/${event.insideTrack.hashtag}`}><h5 className="card-title ok-title">{event.insideTrack.name}</h5></Link>
+            </div>
+            <div className="card-footer">
+                <small className="text-muted">Date: {Moment(event.date).format('D MMM YYYY')}</small>
             </div>
           </div>
-        </Link>
-        ))}
+        </div>
+        <div className="col-sm-12 col-md-9">
+          <div className="card-deck">
+          {event.sessions.map(session => (
+            <div className="card ok-card " key={session.id}>
+              <img className="card-img-top" src={ getYtThumbnailUrl(session.recordingUrl) } alt="Video thumbnail"/>
+              <div className="card-body" key={session.id}>
+                  <p className="card-text"><small className="text-muted">{session.title}</small></p>
+              </div>
+            </div>
+          ))}
+          </div>
         </div>
       </div>
-    </div>
-    ))}
-  </Layout>
-)
+      ))}
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query latestEventsQuery {
