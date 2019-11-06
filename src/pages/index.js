762c3089b73196abdfff163cadad2aa7c7d57fc3
Colin Krawchuk <colin@coling.net>
2019-11-06 21:08:26 +0100

    opensit redesign

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,40 +12,42 @@ const IndexPage = ({ data }) => {
     var eventDate = new Date(event.date)
     event.year = eventDate.getFullYear()
   })
-
   return (
     <Layout>
     <SEO title="Home" />
-      <div className="row mb-3">
+        <div className="row mb-3">
         <div className="col-12">
           <h2>Recent SAP Inside Tracks</h2>
         </div>
       </div>
       {data.gcms.events.map(event => (
       <div className="row mb-5" key={event.id}>
-        <div className="col-sm-12 col-md-3 mb-3">
-          <div className="card">
-            <div className="card-body">
-              <Link to={`/${getSlug(event.insideTrack.hashtag)}`}><h5 className="card-title ok-title">{event.insideTrack.name}</h5></Link>
+        <div className="col">
+          <div className="row mb-3 px-3 pt-2">
+            <div className="d-inline-flex d-flex align-items-center">
+              <div className=""><img src={event.insideTrack.logo.url} alt="inside track logo"></img></div >
+              <div className="px-3 "><Link to={`/${getSlug(event.insideTrack.hashtag)}`}>{event.insideTrack.name}</Link></div>
+              <div className="text-muted">Date: {Moment(event.date).format('D MMM YYYY')}</div>
             </div>
-            <div className="card-footer">
-                <small className="text-muted">Date: {Moment(event.date).format('D MMM YYYY')}</small>
+            <div className="d-flex ml-auto">
+              <Link className="pt-1" to={`/${getSlug(event.insideTrack.hashtag)}`}>View all</Link>
             </div>
           </div>
-        </div>
-        <div className="col-sm-12 col-md-9">
-          <div className="card-deck">
-          {event.sessions.map(session => (
-          /* @Colin this Link tag fucks up the layout :( */
-          /*<Link to={`/${getSlug(event.insideTrack.hashtag)}/${event.year}/${ getSlug(session.title) }`} key={session.id}>*/
-            <div className="card ok-card " key={session.id}>
-              <img className="card-img-top" src={ getYtThumbnailUrl(session.recordingUrl) } alt="Video thumbnail"/>
-              <div className="card-body" key={session.id}>
-                  <p className="card-text"><small className="text-muted">{session.title}</small></p>
+          <div className="row">
+            <div className="col-sm-12 col-md-12">
+              <div className="card-deck">
+                {event.sessions.map(session => (
+                  <div className="card ok-card " key={session.id}>
+                    <Link to={`/${getSlug(event.insideTrack.hashtag)}/${event.year}/${ getSlug(session.title) }`} key={session.id}>
+                      <img className="card-img-top" src={ getYtThumbnailUrl(session.recordingUrl) } alt="Video thumbnail"/>
+                      <div className="card-body p-0 pt-1" key={session.id}>
+                        <p className="card-text"><span className="text-muted font-weight-bolder">{session.title}</span></p>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
               </div>
             </div>
-          /*</Link>*/
-          ))}
           </div>
         </div>
       </div>
@@ -68,8 +70,16 @@ export const query = graphql`
           id
           name
           websiteUrl
+          logo {
+              url(
+                  transformation: {
+                      image: { resize: { width: 30, height: 30, fit: scale } }
+                  }
+              )
+              mimeType
+          }
         }
-        sessions(first: 4) {
+        sessions(first: 5) {
           title
           speaker
           id
