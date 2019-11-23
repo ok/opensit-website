6ea5fe8780cbd8c959b7a3120e8704485782d9df
Oliver Kohl <o.kohl@sap.com>
2019-11-23 14:16:51 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,6 +9,7 @@ const AboutPage = () => (
     <div className="insideTrack-container">
       <h1>OpenSIT</h1>
       <p>All SAP Inside Track recordings in a single place.</p>
+      <p>OpenSIT was officially launched at #sitNL on the 23.11.2019.</p>
     </div>
   </Layout>
 )
