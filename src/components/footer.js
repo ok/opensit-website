0d9c9e8fd469fb43cb25bdfd387dcaab3cd8e5c1
Oliver Kohl <o.kohl@sap.com>
2020-01-07 17:11:56 +0100

    Made with heart :)

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,28 @@
-import { Link } from "gatsby"
 import React from "react"
+import { Link } from "gatsby"
+import { FaHeart, FaTwitter } from 'react-icons/fa';
+import { IconContext } from "react-icons";
 
 const Footer = () => (
     <footer className="bg-light footer">
       <div className="insideTrack-container">
-        <span><Link to="/about" className="text-secondary">About</Link></span>
-        <span><Link to="/imprint" className="text-secondary">Imprint</Link></span>
-        <span><Link to="/privacy" className="text-secondary">Privacy</Link></span>
-        <span><a href="https://twitter.com/opensitnet" className="text-secondary">Twitter</a></span>
+        <div className="row">
+          <div className="col">
+            <span><Link to="/about" className="text-secondary">About</Link></span>
+            <span><Link to="/imprint" className="text-secondary">Imprint</Link></span>
+            <span><Link to="/privacy" className="text-secondary">Privacy</Link></span>
+          </div>
+          <div className="col text-center footer-heart">
+            <IconContext.Provider value={{ className: "big-icon heart-icon" }}>
+              Made with <FaHeart/> in Berlin
+            </IconContext.Provider>
+          </div>
+          <div className="col text-right">
+            <IconContext.Provider value={{ className: "big-icon" }}>
+              <a href="https://twitter.com/opensitnet" className="text-secondary"><FaTwitter/></a>
+            </IconContext.Provider>
+          </div>
+        </div>
       </div>
     </footer>
 )
