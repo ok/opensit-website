deabbf6f275e2797ac290ade38bbf7b2fc45a680
oliver <gokohl@gmail.com>
2020-11-15 23:54:06 +0100

    fix for hidden pagination

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,16 +6,15 @@ const Footer = () => (
     <footer className="bg-light footer">
       <div className="insideTrack-container">
         <div className="row">
-          <div className="col-3">
+          <div className="col-2">
           </div>
           <div className="col text-center footer-heart text-secondary">
             <IconContext.Provider value={{ className: "heart-icon" }}>
               Made with <FaHeart/> in Berlin
             </IconContext.Provider>
           </div>
-          <div className="col-3 text-right">
+          <div className="col-2 text-right">
             <IconContext.Provider value={{ className: "big-icon twitter-icon" }}>
-              <a href="https://twitter.com/opensitnet" className="text-secondary" aria-label="Link to Twitter"><FaTwitter/></a>
               <a href="https://opensit.net/rss.xml" className="text-secondary" aria-label="Link to RSS Feed"><FaRss/></a>
             </IconContext.Provider>
           </div>
