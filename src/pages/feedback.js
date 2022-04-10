c9f965b2be9be9769f382c3f714607e9a5baf8c5
oliver <54379826+ok@users.noreply.github.com>
2022-04-10 09:47:05 +0200

    honeypot added

--- src/pages/feedback.js
+++ src/pages/feedback.js
@@ -11,8 +11,9 @@ const FeedbackPage = ({ data }) => {
     <Seo title="OpenSIT: Feedback" />
     <div className="insideTrack-container">
       <h3>Feedback</h3>
-      <p className="row g-0 col-lg-6 col-sm-12">We love to hear back from you. If there is anything where we can improve, or information displayed wrong on the site, please let us know!</p>
-      <Form name="feedback" method="POST" action="/thanks/" data-netlify="true">
+      <p className="row g-0 col-lg-6 col-sm-12">We love to hear back from you. If there is anything on the site where we can improve, or information displayed wrong, please let us know!</p>
+      <p className="row g-0 col-lg-6 col-sm-12">You can also connect with us on Twitter:&nbsp;<a href="https://twitter.com/opensitnet" target="_blank" rel="noreferrer">@opensitnet</a></p>
+      <Form name="feedback" method="POST" action="/thanks/" data-netlify="true" netlify-honeypot="bot-field">
         <Form.Group controlId="formBasicEmail" className="row g-0 col-lg-8 col-sm-12">
           <Form.Label>Your Name: </Form.Label>
           <Form.Control type="text" name="name" placeholder="Enter name" sm={4} />
@@ -29,6 +30,12 @@ const FeedbackPage = ({ data }) => {
           <Form.Control as="textarea" name="feedback-text" rows={5} />
         </Form.Group>
         <Button type="submit">Send</Button>
+        <p className="form-hidden">
+          <label>
+            Don’t fill this out if you’re human: <input name="bot-field" />
+          </label>
+        </p>
+        <input type="hidden" name="bot-field" />
         <input type="hidden" name="form-name" value="feedback" />
       </Form>
     </div>
